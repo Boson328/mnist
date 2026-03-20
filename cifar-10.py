3d29e8bffe4a96e0c21146c30fe33422947b5728
Boson <121548464+Boson328@users.noreply.github.com>
2026-03-20 14:04:35 +0900

    適当に

--- cifar-10.py
+++ cifar-10.py
@@ -1,7 +1,8 @@
 import datetime
 import keras
-from keras import layers
+from keras import layers, models
 from typing import cast, Any
+import tensorflow as tf
 
 # 1. データのロード（自動でダウンロードされます）
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -9,6 +10,55 @@ from typing import cast, Any
 # 正規化（0-1の範囲に）
 x_train = x_train.astype("float32") / 255.0
 x_test = x_test.astype("float32") / 255.0
+
+def residual_block_x2(x, filters, strides=1):
+    original_x = x;
+    x = layers.Conv2D(filters=filters, kernel_size=(3, 3), padding='same', strides=strides)(x)
+    x = layers.BatchNormalization()(x) # 正規化
+    x = layers.Activation("mish")(x) # 活性化関数
+
+    x = layers.Conv2D(filters=filters, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x) # 正規化
+
+    original_x = layers.Conv2D(filters=filters, kernel_size=(1, 1), strides=strides)(original_x)
+
+    x = layers.Add()([x, original_x])
+    x = layers.Activation("mish")(x)
+    return x
+
+
+def conv1(x):
+    x = layers.Conv2D(64, (3, 3), padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("mish")(x)
+    return x
+
+def conv2_x(x):
+    # x = layers.MaxPooling2D((3, 3), strides=2, padding="same")(x)
+    x = residual_block_x2(x, 64)
+    x = residual_block_x2(x, 64)
+    return x
+
+def conv3_x(x):
+    x = residual_block_x2(x, 128, 2)
+    x = residual_block_x2(x, 128)
+    x = residual_block_x2(x, 128)
+    return x
+
+def conv4_x(x):
+    x = residual_block_x2(x, 256, 2)
+    x = residual_block_x2(x, 256)
+    x = residual_block_x2(x, 256)
+    return x
+
+def conv5_x(x):
+    x = residual_block_x2(x, 512, 2)
+    x = residual_block_x2(x, 512)
+    x = residual_block_x2(x, 512)
+    return x
+
+
+
 
 # 2. モデル構築（カラー写真用に少し深くします）
 def build_cifar_model():
@@ -22,33 +72,30 @@ def build_cifar_model():
         keras.layers.RandomZoom(0.08),
     ])
 
-    model = keras.Sequential([
-        layers.Input(shape=(32, 32, 3)), # RGBの3チャンネル！
-        data_augmentation,
-        
-        # 第1ブロック
-        layers.Conv2D(32, (3, 3), padding='same', activation='mish'),
-        layers.BatchNormalization(),
-        layers.Conv2D(64, (3, 3), padding='same', activation='mish'),
-        layers.BatchNormalization(),
-        layers.MaxPooling2D((2, 2)),
-        layers.Dropout(0.2),
+    inputs = layers.Input(shape=(32, 32, 3)) # RGBの3チャンネル！
+    x = data_augmentation(inputs)
 
-        # 第2ブロック（チャンネル数を増やして特徴を捉える）
-        layers.Conv2D(128, (3, 3), padding='same', activation='mish'),
-        layers.BatchNormalization(),
-        layers.Conv2D(128, (3, 3), padding='same', activation='mish'),
-        layers.BatchNormalization(),
-        layers.MaxPooling2D((2, 2)),
-        layers.Dropout(0.3),
+    # conv1の部分
+    x = conv1(x)
 
-        layers.Flatten(),
-        layers.Dense(128, activation='mish'),
-        layers.BatchNormalization(),
-        layers.Dropout(0.4),
-        layers.Dense(10, activation='softmax')
-    ])
+    x = conv2_x(x)
+    x = conv3_x(x)
+
+    x = conv4_x(x)
+    # 8, 8, 64
+    x = layers.GlobalAveragePooling2D()(x)
+
+    x = layers.Dense(256)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("mish")(x)
+
+    x = layers.Dropout(0.5)(x)
+
+    outputs = layers.Dense(10, activation='softmax')(x)
+    model = models.Model(inputs=inputs, outputs=outputs)
+
     return model
+
 
 model = build_cifar_model()
 
